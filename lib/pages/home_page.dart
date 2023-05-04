a4cca764712797ac3b464c005280dd544a10d232
lzlocki <lzlocki@poczta.fm>
2023-05-04 21:14:32 +0200

    Create main page and first task objects

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/util/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+      title: Text('To Do'),
+        elevation: 3,
+    ),
+        body: ListView(
+        children: [
+          ToDoTile(
+            taskName: "make tutorial1",
+            taskCompleted: true,
+            onChanged: (p0) {},
+          ),
+      ],
+    ),
+    );
   }
 }
