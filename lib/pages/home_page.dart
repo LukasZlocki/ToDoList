a8a4ad5d3d113e8c6254e86224760c6cbf846a29
lzlocki <lzlocki@poczta.fm>
2023-05-04 21:34:46 +0200

    Add dynamic checkbox, list with tasks

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // list of todos tasks
+  List toDoList = [
+    ["Make tutorial", false],
+    ["Do excercise", false]
+  ];
+
+  // tapped checkbox method
+  void checkBoxChanged( bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,16 +30,17 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
       title: Text('To Do'),
         elevation: 3,
-    ),
-        body: ListView(
-        children: [
-          ToDoTile(
-            taskName: "make tutorial1",
-            taskCompleted: true,
-            onChanged: (p0) {},
-          ),
-      ],
-    ),
-    );
+      ),
+        body: ListView.builder(
+          itemCount: toDoList.length,
+          itemBuilder: (context, index) {
+            return ToDoTile(
+                taskName: toDoList[index][0],
+                taskCompleted: toDoList[index][1],
+                onChanged: (value) => checkBoxChanged(value, index),
+            );
+          }
+        )
+      );
   }
 }
